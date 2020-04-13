ca40721909a033cebd9a98013a617d32e59bacc3
adithwip <adithwidyapradipta@gmail.com>
2020-04-13 19:34:52 +0700

    add PrismJS and implementation

--- src/pages/FibonacciSequence/components.tsx
+++ src/pages/FibonacciSequence/components.tsx
@@ -6,10 +6,43 @@ import Button from "../../components/Button/component";
 import Paragraph from "../../components/Paragraph/component";
 import Input from "../../components/Input/component";
 import TextBox from "../../components/TextBox/component";
+import PrismJS from "../../components/PrismJS/components";
 
 import { findFibonacci } from "../../services/fibonacciSequence/index";
 
 import * as styles from "./fibonacci-sequence.styles";
+
+const tsCode: string = `
+function findFibonacci(n: number): number[] {
+  let numbers: number[] = [];
+
+  for (let i = 0; i < n; i++) {
+    if (i <= 1) {
+      numbers.push(i);
+    } else {
+      numbers.push(numbers[i - 1] + numbers[i - 2]);
+    }
+  }
+
+  return numbers;
+};
+`.trim();
+
+const jsCode: string = `
+function findFibonacci(n) {
+  let numbers = [];
+
+  for (let i = 0; i < n; i++) {
+    if (i <= 1) {
+      numbers.push(i);
+    } else {
+      numbers.push(numbers[i - 1] + numbers[i - 2]);
+    }
+  }
+
+  return numbers;
+};
+`.trim();
 
 const FibonacciSequence: React.FC = () => {
   const [a, setA] = useState(0);
@@ -30,12 +63,23 @@ const FibonacciSequence: React.FC = () => {
       <Flex justifyContent="center">
         <h1 css={styles.headerTitle}>Fibonacci Sequence</h1>
       </Flex>
+
       <Flex mt={3} mb={4} align="center">
         <h3 css={styles.headerTitle}>
           A function that returns fibonacci sequence from n as maximum distance.
         </h3>
       </Flex>
-      <Flex>This will be a code block container</Flex>
+
+      <Flex flexDirection="column">
+        <label>TypeScript</label>
+        <PrismJS code={tsCode} language="ts" plugins={["show-language"]} />
+      </Flex>
+
+      <Flex flexDirection="column">
+        <label>JavaScript</label>
+        <PrismJS code={jsCode} language="js" plugins={["show-language"]} />
+      </Flex>
+
       <Flex mt={3} flexDirection="column">
         <Paragraph size="1.2rem" mb={3}>
           Given a number as an argument. the function will generate fibonacci
@@ -47,12 +91,14 @@ const FibonacciSequence: React.FC = () => {
           than a number, it will error.
         </Paragraph>
       </Flex>
+
       <Flex mt={5} flex={1}>
         <Flex flexDirection="column" flex={1}>
           <Input mb={3} placeholder="Number" onChange={handleChange} />
           <Button label="Run Code" onClick={handleClick} />
         </Flex>
       </Flex>
+
       <Flex mt={3}>
         <TextBox>{`[${result}]`}</TextBox>
       </Flex>
--- src/pages/PrimeNumberSequence/component.tsx
+++ src/pages/PrimeNumberSequence/component.tsx
@@ -6,10 +6,67 @@ import Button from "../../components/Button/component";
 import Paragraph from "../../components/Paragraph/component";
 import Input from "../../components/Input/component";
 import TextBox from "../../components/TextBox/component";
+import PrismJS from "../../components/PrismJS/components";
 
 import { primeNumSequence } from "../../services/primeNumberSequence/index";
 
 import * as styles from "./prime-number-sequence.styles";
+
+const tsCode: string = `
+function isPrime(n: number): boolean {
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+function primeNumSequence(n: number): number[] {
+  let primes: number[] = [];
+
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)) {
+      primes.push(i);
+    }
+  }
+
+  return primes;
+};
+`.trim();
+
+const jsCode: string = `
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+function primeNumSequence(n) {
+  let primes = [];
+
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)) {
+      primes.push(i);
+    }
+  }
+
+  return primes;
+};
+`.trim();
 
 const PrimeNumberSequence: React.FC = () => {
   const [a, setA] = useState(0);
@@ -30,13 +87,24 @@ const PrimeNumberSequence: React.FC = () => {
       <Flex justifyContent="center">
         <h1 css={styles.headerTitle}>Prime Number Sequence</h1>
       </Flex>
+
       <Flex mt={3} mb={4} align="center">
         <h3 css={styles.headerTitle}>
           A function that returns prime number sequence from n as maximum
           distance.
         </h3>
       </Flex>
-      <Flex>This will be a code block container</Flex>
+
+      <Flex flexDirection="column">
+        <label>TypeScript</label>
+        <PrismJS code={tsCode} language="ts" plugins={["show-language"]} />
+      </Flex>
+
+      <Flex flexDirection="column">
+        <label>JavaScript</label>
+        <PrismJS code={jsCode} language="js" plugins={["show-language"]} />
+      </Flex>
+
       <Flex mt={3} flexDirection="column">
         <Paragraph size="1.2rem" mb={3}>
           Given a number as an argument. the function will find every prime
@@ -48,12 +116,14 @@ const PrimeNumberSequence: React.FC = () => {
           than a number, it will error.
         </Paragraph>
       </Flex>
+
       <Flex mt={5} flex={1}>
         <Flex flexDirection="column" flex={1}>
           <Input mb={3} placeholder="Number" onChange={handleChange} />
           <Button label="Run Code" onClick={handleClick} />
         </Flex>
       </Flex>
+
       <Flex mt={3}>
         <TextBox>{`[${result}]`}</TextBox>
       </Flex>
